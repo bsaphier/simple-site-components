ca05e20e9eeb3fc1123600fa0a99acc6ed4482af
Benjamin Saphier <b.saphier@gmail.com>
2017-10-23 23:14:15 -0400

    title is same as on cards

--- src/lib/design/Title.jsx
+++ src/lib/design/Title.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import * as s from './title.scss';
 
 
-export default ({children, ...props}) => (
-    <div className={s.titleWrapper} {...props}>
-        <div className={s.titleText}>
+export default ({children, className, style, ...props}) => (
+    <div className={`${s.titleWrapper}`} {...props}>
+        <div className={s.titleText} style={{...style}}>
             { children }
         </div>
     </div>
