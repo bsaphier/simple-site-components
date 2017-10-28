71c34f2a05ee85a981cee5a3b1c0be8fdfa3fbea
Benjamin Saphier <b.saphier@gmail.com>
2017-10-28 16:32:23 -0400

    cards inherit font-family

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -129,22 +129,29 @@ export default class App extends React.Component {
                         {renderCards(2, 3)}
                     </SSC.SideMenu>
                     <SSC.PageContent style={{background: '#fefefe'}}>
-                        <SSC.Card title={'Card Title'}>
+                        <SSC.Card title={'Card Title'} expandable>
                             {() => [
                                 <SSC.Btn key={'btna002'} type={'B'} action={this.toggleBurger}>{() => 'Show Burger'}</SSC.Btn>,
                                 <SSC.Btn key={'btnb002'} type={'B'} action={this.toggleModal}>{() => 'Open Modal'}</SSC.Btn>
                             ]}
                         </SSC.Card>
+                        <span className={s.customCard}>
+                            <SSC.Card title={'EXPANDABLE Card'} expandable>
+                                {(clicked, isFoot) => {
+                                    if (!isFoot) {
+                                        return (
+                                            <div className={clicked ? `${s.cardContent} ${s.clicked}` : s.cardContent}>
+                                                <SSC.Title className={s.customTitle} style={{ background: 'rgba(0,0,255,0.1)' }}>{'A Custom SSC.Title'}</SSC.Title>
+                                                {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'}
+                                            </div>
+                                        );
+                                    } else {
+                                        return (<div className={s.customCardFooter}>{'This is a Custom Footer!'}</div>);
+                                    }
+                                }}
+                            </SSC.Card>
+                        </span>
                         {renderCards(5, 1)}
-                        <SSC.Card title={'EXPANDABLE Card'} expandable>
-                            {(clicked, isFoot) => {
-                                if (!isFoot) {
-                                    return <div className={clicked ? `${s.cardContent} ${s.clicked}` : s.cardContent}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</div>;
-                                } else {
-                                    return (<div style={{margin: 'auto'}}>{'This is a Custom Footer!'}</div>);
-                                }
-                            }}
-                        </SSC.Card>
                     </SSC.PageContent>
                     <SSC.Modal open={this.state.modalOpen} close={this.toggleModal}>
                         {renderCards(1, 3)}
--- src/lib/design/Title.jsx
+++ src/lib/design/Title.jsx
@@ -3,7 +3,7 @@ import * as s from './title.scss';
 
 
 export default ({children, className, style, ...props}) => (
-    <div className={`${s.titleWrapper}`} {...props}>
+    <div className={`${s.titleWrapper} ${className}`} {...props}>
         <div className={s.titleText} style={{...style}}>
             { children }
         </div>
